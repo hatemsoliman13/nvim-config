e6428e2514c1358c4cac3d5be9c1419e89524a16
DonKeem <karim.m.s.soliman@gmail.com>
2024-05-14 06:40:31 +0300

    updating nvim configs
    - removed git signs
    - adding configs to lsp
    - adding configs to neo-tree

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "psalm", "tsserver","intelephense"  },
+				ensure_installed = { "lua_ls", "tsserver", "intelephense" },
 			})
 		end,
 	},
@@ -17,12 +17,14 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.intelephense.setup({ capabilities = capabilities })
 			lspconfig.tsserver.setup({ capabilities = capabilities })
 			vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, {})
 			vim.keymap.set("n", "<leader>lca", vim.lsp.buf.code_action, {})
 		end,
 	},
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,15 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		local neotree = require("neo-tree")
+		neotree.setup({
+			close_if_last_window = true,
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+		})
 		vim.keymap.set("n", "<leader>nn", ":Neotree filesystem focus toggle left<CR>", {})
 		vim.keymap.set("n", "<leader>nb", ":Neotree buffers focus toggle float<CR>", {})
 		vim.keymap.set("n", "<leader>ng", ":Neotree git_status focus toggle float<CR>", {})
