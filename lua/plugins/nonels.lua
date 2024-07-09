ac83fd23db5261944452a7fbfaa6a9863d12258f
DonKeem <karim.m.s.soliman@gmail.com>
2024-07-09 11:16:35 +0300

    removing javascript lsps lint format etc

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -9,10 +9,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.phpcsfixer,
 				null_ls.builtins.diagnostics.phpstan,
-				require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 		vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, {})
