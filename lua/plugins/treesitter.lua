aa2141b02b2f09e25aacf894650a074339ee2216
hatemsoliman13 <hatem.soliman333@gmail.com>
2026-01-30 13:38:12 +0200

    fix treesitter configs error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,11 +2,11 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		local config = require("nvim-treesitter.configs")
+		local config = require("nvim-treesitter.config")
 		config.setup({
 			ensure_installed = { "lua", "php", "phpdoc", "javascript", "typescript", "tsx", "css", "json", "html" },
 			highlight = { enable = true },
-			-- indent = { enable = true },
+			indent = { enable = true },
 		})
 	end,
 }
