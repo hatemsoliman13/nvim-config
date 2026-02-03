044b708bdb2280b6bacca8f65d7af7fc35e957d0
Hatem Soliman <hatem.soliman333@gmail.com>
2026-02-03 21:27:21 +0200

    fix conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,20 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "ruff" },
-			javascript = { "prettier" },
-			php = { "php_cs_fixer" },
-			sh = { "shfmt" },
-			bash = { "shfmt" },
-		},
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-	},
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "ruff" },
+            typescript = { "prettier" },
+            typescriptreact = { "prettier" },
+            javascript = { "prettier" },
+            javascriptreact = { "prettier" },
+            php = { "php_cs_fixer" },
+            sh = { "shfmt" },
+            bash = { "shfmt" },
+        },
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
+    },
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,23 +1,24 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		local neotree = require("neo-tree")
-		neotree.setup({
-			close_if_last_window = true,
-			filesystem = {
-				follow_current_file = {
-					enabled = true,
-				},
-			},
-		})
-		vim.keymap.set("n", "<leader>nn", ":Neotree filesystem focus toggle left<CR>", {})
-		vim.keymap.set("n", "<leader>nb", ":Neotree buffers focus toggle float<CR>", {})
-		vim.keymap.set("n", "<leader>ng", ":Neotree git_status focus toggle float<CR>", {})
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    lazy = false,
+    config = function()
+        local neotree = require("neo-tree")
+        neotree.setup({
+            close_if_last_window = true,
+            filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                },
+            },
+        })
+        vim.keymap.set("n", "<leader>nn", ":Neotree filesystem focus toggle left<CR>", {})
+        vim.keymap.set("n", "<leader>nb", ":Neotree buffers focus toggle float<CR>", {})
+        vim.keymap.set("n", "<leader>ng", ":Neotree git_status focus toggle float<CR>", {})
+    end,
 }
