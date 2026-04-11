9912f6d29fefe8ab220f5281536f36d8204b9f91
Hatem Mohamed <hatem.mohamed@trypass.ai>
2026-04-11 11:03:51 +0200

    new tools

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        local config = require("nvim-treesitter.configs")
+        local config = require("nvim-treesitter.config")
         config.setup({
             ensure_installed = { "lua", "php", "phpdoc", "javascript", "typescript", "tsx", "css", "json", "html" },
             highlight = { enable = true },
